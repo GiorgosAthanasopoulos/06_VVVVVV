a96e562c553ba37d27fda6cdc049a3213b845e87
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-07-21 18:33:18 +0300

    add credits scene 1

---
+++ scripts/ui/credits/credits.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+@export var next_page_scene_res_path: String = "res://scenes/ui/credits/credits_2.tscn"
+@export var last_page_scene_res_path: String = "res://scenes/ui/credits/credits_22.tscn"
+
+
+func _ready() -> void:
+	modulate = ColorController.get_next_color()
+
+
+func _on_return_button_pressed() -> void:
+	if ! await SceneManager.goto_scene(SceneManager.main_menu_scene_res_path):
+		SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
+
+
+func _on_next_page_button_pressed() -> void:
+	if ! await SceneManager.goto_scene(next_page_scene_res_path):
+		SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
+
+
+func _on_last_page_button_pressed() -> void:
+	if ! await SceneManager.goto_scene(last_page_scene_res_path):
+		SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
