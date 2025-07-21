8264a9ed0a2e04f7ebc0a92c22cf49ba265b5810
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-07-21 16:33:13 +0300

    modified 'next' scenes for navigation in loading/pre main menu

--- scripts/ui/pre_main_menu/pre_main_menu.gd
+++ scripts/ui/pre_main_menu/pre_main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 @export var action_input_map: String = "ui_accept"
-@export var main_menu_scene_res_path: PackedScene
+@export var main_menu_scene_res_path: PackedScene = preload("res://scenes/ui/main_menu.tscn")
 
 
 func _process(_delta: float) -> void:
