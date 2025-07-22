4a7acfa2b176058fd310b22e7b7eec53c182ce88
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-07-22 17:25:10 +0300

    Made generic scirpt for credits since behavior doesnt change much

--- scripts/ui/credits/credits.gd
+++ scripts/ui/credits/credits.gd
@@ -2,11 +2,19 @@ extends Control
 
 
 @export var next_page_scene_res_path: String = "res://scenes/ui/credits/0-10/credits_2.tscn"
-@export var last_page_scene_res_path: String = "res://scenes/ui/credits/21-30/credits_22.tscn"
+@export var previous_page_scene_res_path: String = "res://scenes/ui/credits/21-30/credits_22.tscn"
+
+@export var next_button: Button
+@export var previous_button: Button
+@export var return_button: Button
 
 
 func _ready() -> void:
 	modulate = ColorController.get_next_color()
+
+	var __: Error = return_button.pressed.connect(_on_return_button_pressed) as Error
+	__ = previous_button.pressed.connect(_on_last_page_button_pressed) as Error
+	__ = next_button.pressed.connect(_on_next_page_button_pressed) as Error
 
 
 func _on_return_button_pressed() -> void:
@@ -22,6 +30,6 @@ func _on_next_page_button_pressed() -> void:
 
 
 func _on_last_page_button_pressed() -> void:
-	if ! await SceneManager.goto_scene(last_page_scene_res_path):
+	if ! await SceneManager.goto_scene(previous_page_scene_res_path):
 		pass
 		#SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
