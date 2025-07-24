eb358911b517b998ebdc8e85a2d369cfa5991d5b
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-07-24 16:21:24 +0300

    escape while in credits goes back to main menu

--- scripts/ui/credits/credits.gd
+++ scripts/ui/credits/credits.gd
@@ -8,6 +8,8 @@ extends Control
 @export var previous_button: Button
 @export var return_button: Button
 
+@export var back_input_map: String = "ui_cancel"
+
 
 func _ready() -> void:
 	modulate = ColorController.get_next_color()
@@ -17,10 +19,13 @@ func _ready() -> void:
 	__ = next_button.pressed.connect(_on_next_page_button_pressed) as Error
 
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed(back_input_map):
+		goto_main_menu()
+
+
 func _on_return_button_pressed() -> void:
-	if ! await SceneManager.goto_scene(SceneManager.main_menu_scene_res_path):
-		pass
-		#SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
+	goto_main_menu()
 
 
 func _on_next_page_button_pressed() -> void:
@@ -31,5 +36,11 @@ func _on_next_page_button_pressed() -> void:
 
 func _on_last_page_button_pressed() -> void:
 	if ! await SceneManager.goto_scene(previous_page_scene_res_path):
+		pass
+		#SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
+
+
+func goto_main_menu() -> void:
+	if ! await SceneManager.goto_scene(SceneManager.main_menu_scene_res_path):
 		pass
 		#SceneManager.quit(SceneManager.ERROR_EXIT_CODE)
