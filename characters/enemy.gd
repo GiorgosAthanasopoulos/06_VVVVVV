8f85b48df66c51c624564cb4765bd7a913ad228d
Giorgos Athanasopoulos <spzdoom@gmail.com>
2026-09-16 20:35:10 +0300

    backup

--- characters/enemy.gd
+++ characters/enemy.gd
@@ -6,16 +6,12 @@ extends CharacterBody2D
 # TODO: checkpoint to respawn in after dying in every room (light up when interacted)
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := 0.0
 	if direction:
 		velocity.x = direction * SPEED
 	else:
